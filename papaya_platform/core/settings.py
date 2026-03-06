d91b5934c81f30f10b2b796c1c7325b5d0603820
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-06 16:36:39 +0000

    chore: add configuration for django unfold

--- papaya_platform/core/settings.py
+++ papaya_platform/core/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django unfold for admin panel
+    "unfold",
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
