2758178ee0356346a0f9903d7349fd7d4f65553d
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-10 17:47:33 +0000

    write base definition for auth handling views

--- papaya_platform/apps/accounts/views.py
+++ papaya_platform/apps/accounts/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+
+
 # Create your views here.
+
+def system_register(request: HttpRequest) -> HttpResponse:
+    return render(request, "accounts/register.html")
+
+
+def system_login(request: HttpRequest) -> HttpResponse:
+    return render(request, "accounts/login.html")
+
+
+def system_log_out(request: HttpRequest) -> HttpResponse:
+    pass
