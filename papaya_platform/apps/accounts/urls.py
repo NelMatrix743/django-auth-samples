a70ea3f71995886ffe28b7e403d38a34c8b34e64
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-10 17:47:50 +0000

    write url conf for auth views

--- papaya_platform/apps/accounts/urls.py
+++ papaya_platform/apps/accounts/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from .views import system_register, system_login, system_log_out
+
 
 
 app_name: str = "accounts"
 
-urlpatterns: list = []
+urlpatterns: list = [
+    path("register/", system_register, name="register"),
+    path("login/", system_login, name="login"),
+    path("logout/", system_log_out, name="logout"),
+]
