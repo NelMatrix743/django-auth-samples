6aee5b440bf38914eeabd4d25040a87036e4b336
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-12 18:31:03 +0000

    define url conf for system home view

--- papaya_platform/apps/home/urls.py
+++ papaya_platform/apps/home/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from .views import system_home_view
+
 
 
 app_name: str = "home"
 
-urlpatterns: list = []
+urlpatterns: list = [
+    path('', system_home_view, name="home"),
+]
