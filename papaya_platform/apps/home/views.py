673bd8dff6ffe4ab44c4b07363c473b70db30718
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-12 18:30:48 +0000

    write core structure for system home view

--- papaya_platform/apps/home/views.py
+++ papaya_platform/apps/home/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+
+
 # Create your views here.
+
+def system_home_view(request: HttpRequest) -> HttpResponse: 
+    return render(request, "home/index.html")
