8abf3201ab182e15008b3a7935438aab66330945
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-13 08:47:24 +0000

    add login_required mixin to enforce login for unauthenticated users

--- papaya_platform/apps/home/views.py
+++ papaya_platform/apps/home/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 
-# Create your views here.
-
+@login_required
 def system_home_view(request: HttpRequest) -> HttpResponse: 
     return render(request, "home/index.html")
+
+# eosc
